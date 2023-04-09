e6af2942128691dafea4844a91fa89494dfef3d5
Midlyx Hatrys <galactic.koreiko@gmail.com>
2023-04-09 23:32:02 +0300

    rewrite

--- README.md
+++ README.md
@@ -1,47 +1,11 @@
 # hoon-assist-emacs
-Provide official documentation for Hoon runes and commands under point. 
+Provide official documentation for Hoon runes and stdlib arms at point.
 
 ## Installation
-
-Installation is manual.
-
-1. Install  [hoon-mode.el](https://github.com/urbit/hoon-mode.el).
-
-2. `git clone https://github.com/poctyr-bidbes/hoon-assist-emacs.git`
-
-3. Move the following files into your ~/.emacs.d/site-lisp/ directory:
-
-	hoon-assist-emacs.el<br>
-	hoon-dictionary.json
-
-4. Add a hoon-mode hook to your init.el file
-
-```
-(defun hoon-assist-hook ()
-  (progn
-    (global-set-key (kbd "<f7>") 'get-token-definition)
-    (setq site-lisp-dir "~/.emacs.d/site-lisp/")
-    (setq dict-file (concat site-lisp-dir "hoon-dictionary.json"))
-    (load "hoon-assist-emacs")
-    (message "hoon-assist loaded")))
-
-(add-hook 'hoon-mode-hook 'hoon-assist-hook )
-```
+(TBD)
 
 ## Operation
-
-Restart emacs, open a hoon file.
-
-Place point on the rune or command you would like defined. Press F7. Press F7 again to close the documentation window.
+(TBD)
 
 ## Notes
-
-Modify the hook function to utilize a different function key. Alternatively you can `M-x get-token-definition`.
-
-If your site-lisp directory differs from `~/.emacs.d/site-lisp` you will need to edit the `site-lisp-dir` variable in the hook method.
-
-3 minute [Youtube video](https://youtu.be/BY4O-g9IwAY)
-
 Dictionary source: https://github.com/urbit/hoon-assist-vscode/tree/main/src/lib
-
-Tested with GNU Emacs 27.1 on Ubuntu 22.04.2 LTS, 8GB RAM 
---
+++ hoon-assist.el
@@ -0,0 +1,61 @@
+;;; -*- lexical-binding: t -*-
+(require 'json)
+(require 'shr)
+
+;;;###autoload
+(defun hoon-assist-definition (term)
+  "Display information about the Hoon term under point.
+
+With a prefix arg, or if there is no \"thing\" at point, prompt
+for the term to show information about."
+  (interactive (list (hoon-assist--thing-at-point)))
+  (let ((definition (hoon-assist--definition term)))
+    (if (null definition)
+        (message "nothing for %s, sorry!" term)
+
+      (or (fboundp 'libxml-parse-html-region)
+          (error "This function requires Emacs to be compiled with libxml2"))
+
+      (pop-to-buffer (generate-new-buffer "*hoon-assist*"))
+
+      (hoon-assist-popup-mode)
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (shr-insert-document
+         (with-temp-buffer
+           (insert definition)
+           (libxml-parse-html-region (point-min) (point-max)))))
+      (goto-char (point-min)))))
+
+(let ((dictionary nil))
+  (defun hoon-assist--ensure-dictionary ()
+    (unless dictionary
+      (let* ((this-file (symbol-file 'hoon-assist--ensure-dictionary))
+             (dictionary-file (concat (file-name-directory this-file) "hoon-dictionary.json"))
+             (definitions (let ((json-object-type 'alist)
+                                (json-array-type 'list))
+                            (json-read-file dictionary-file)))
+             (table (make-hash-table :test 'equal)))
+        (dolist (definition definitions)
+          (dolist (key (cdr (assq 'keys definition)))
+            (puthash key (cdr (assq 'doc definition)) table)))
+
+        (setq dictionary table)))
+    dictionary))
+
+(defun hoon-assist--definition (key)
+  (gethash key (hoon-assist--ensure-dictionary)))
+
+(define-derived-mode hoon-assist-popup-mode special-mode "Hoon Assist Popup"
+  "Mode for Hoon Assist popup help buffers."
+  (setq buffer-read-only t))
+
+(defun hoon-assist--thing-at-point ()
+  (let* ((at-point (symbol-at-point)))
+    (if (or current-prefix-arg (null at-point))
+        (completing-read "Term: "
+                         (hash-table-keys (hoon-assist--ensure-dictionary))
+                         nil t nil nil at-point)
+      at-point)))
+
+(provide 'hoon-assist)
